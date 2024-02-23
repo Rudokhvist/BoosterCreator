2fcaa1788c123001f972e9ac525957733abac6f0
Rudokhvist <kotlyar.andrey@gmail.com>
2024-02-23 16:30:37 +0200

    add suppresions file

---
+++ BoosterCreator/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Performance", "SYSLIB1045:Convert to 'GeneratedRegexAttribute'.", Justification = "<Pending>", Scope = "member", Target = "~M:BoosterCreator.BoosterHandler.CreateBooster(ArchiSteamFarm.Steam.Bot,System.Collections.Concurrent.ConcurrentDictionary{System.UInt32,System.Nullable{System.DateTime}})~System.Threading.Tasks.Task{System.String}")]
